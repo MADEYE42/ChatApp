d30933996431f2b5b014c48ce097a6b97e53d33d
Gouresh  Madye <117588160+MADEYE42@users.noreply.github.com>
2024-06-05 13:14:51 +0530

    All the css is complete and server.js has functions initializedd

--- server.js
+++ server.js
@@ -5,5 +5,19 @@ const path = require("path");
 const app = express();
 const server = require("http").createServer(app);
 
+const io = require("socket.io")(server);
+
 app.use(express.static(path.join(__dirname+"/public")));
+
+io.on("connection",function(socket){
+    socket.on("newuser",function(username){
+        socket.broadcast.emit("update","Be aware "+ username + " has arrived.")
+    })
+    socket.on("exituser",function(username){
+        socket.broadcast.emit("update",username + "has left,back bitch about him.")
+    })
+    socket.on("chat",function(message){
+        socket.broadcast.emit("chat",message);
+    })
+});
 server.listen(5000);
